c9cb0f6c17f1e6c8bb466ed7d1242ef294f9b198
BDookie02 <jojo.sisco123@gmail.com>
2026-08-10 09:47:10 -0400

    Prepare Unity v0.4.4 cross-platform build

--- Assets/WOF/Editor/WofProjectAutomation.cs
+++ Assets/WOF/Editor/WofProjectAutomation.cs
@@ -227,8 +227,8 @@ namespace WOF.Editor
         {
             PlayerSettings.companyName = "Wizards Only Fools";
             PlayerSettings.productName = "Wizards Only Fools";
-            PlayerSettings.bundleVersion = "0.4.3";
-            PlayerSettings.Android.bundleVersionCode = 6;
+            PlayerSettings.bundleVersion = "0.4.4";
+            PlayerSettings.Android.bundleVersionCode = 7;
             PlayerSettings.runInBackground = true;
             PlayerSettings.colorSpace = ColorSpace.Linear;
             PlayerSettings.defaultScreenWidth = 1280;
