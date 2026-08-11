7a94a0fa64b8cc42e3a30d6c137861c9b9ffee20
BDookie02 <jojo.sisco123@gmail.com>
2026-08-11 10:00:21 -0400

    Publish Unity v0.4.13 controller menu build

--- Assets/WOF/Editor/WofProjectAutomation.cs
+++ Assets/WOF/Editor/WofProjectAutomation.cs
@@ -227,8 +227,8 @@ namespace WOF.Editor
         {
             PlayerSettings.companyName = "Wizards Only Fools";
             PlayerSettings.productName = "Wizards Only Fools";
-            PlayerSettings.bundleVersion = "0.4.12";
-            PlayerSettings.Android.bundleVersionCode = 15;
+            PlayerSettings.bundleVersion = "0.4.13";
+            PlayerSettings.Android.bundleVersionCode = 16;
             PlayerSettings.runInBackground = true;
             PlayerSettings.colorSpace = ColorSpace.Linear;
             PlayerSettings.defaultScreenWidth = 1280;
